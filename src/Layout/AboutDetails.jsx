3342ef98155e6f2f5ba364b6d3a9aa61e7351e2c
Jessy <ayy.jessy@gmail.com>
2023-07-31 16:12:37 +0200

    collapse / animation

--- src/Layout/AboutDetails.jsx
+++ src/Layout/AboutDetails.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 
 function AboutDetails() {
-  const [reliabilityDisplayed, setReliabilityDisplayed] = useState()
+  const [reliabilityDisplayed, setReliabilityDisplayed] = useState();
+  const [respectDisplayed, setRespectDisplayed] = useState();
+  const [serviceDisplayed, setServiceDisplayed] = useState();
+  const [securityDisplayed, setSecurityDisplayed] = useState();
   
   return (
     <div className="aboutus-page">
@@ -19,23 +22,23 @@ function AboutDetails() {
             <div className='About-page__respect'>
                 <p className='respect__title'>
                     Respect
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i onClick={() => setRespectDisplayed(!respectDisplayed)} className={respectDisplayed ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'}></i>
                     </p>
-                <p className='respect__content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
+                <p className={respectDisplayed ? 'respect__content' : 'hidden'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
             </div>
             <div className="About-page__service">
                 <p className="service__title">
                     Service
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i onClick={() => setServiceDisplayed(!serviceDisplayed)} className={serviceDisplayed ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'}></i>
                 </p>
-                <p className='service__content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
+                <p className={serviceDisplayed ? 'service__content' : 'hidden'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
             </div>
             <div className="About-page__securite">
                 <p className="securite__title">
                     Securité
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i onClick={() => setSecurityDisplayed(!securityDisplayed)} className={securityDisplayed ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'}></i>
                 </p>
-                <p className='securite__content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
+                <p className={securityDisplayed ? 'securite__content' : 'hidden'}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
             </div>
         </div>
     </div>
--- src/Layout/AppartementsDetails.jsx
+++ src/Layout/AppartementsDetails.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
-function AppartementsDetails() {
+function AppartementsDetails({ appartment }) {
+    const [descriptionDisplayed, setDescriptionDisplayed] = useState();
+    const [equipmentsDisplayed, setEquipmentsDisplayed] = useState();
+
+    
   return (
     <div className='Appartements-page'>
         <div className="Menu_Colapse">
             <div className='Appartements-page__description'>
                 <p className='description__title'>
                     Description
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i onClick={() => setDescriptionDisplayed(!descriptionDisplayed)} className={descriptionDisplayed ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'}></i>
                 </p>
-                <p className='description__content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
+                <p className={descriptionDisplayed ? 'description__content' : 'hidden'}>{appartment.description}</p>
             </div>
             <div className='Appartements-page__equipements'>
                 <p className='equipements__title'>
                     Equipements
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i onClick={() => setEquipmentsDisplayed(!equipmentsDisplayed)} className={equipmentsDisplayed ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'}></i>
                     </p>
-                <p className='equipements__content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci perspiciatis iste sunt, distinctio laborum necessitatibus doloremque est? Mollitia inventore maiores praesentium soluta, illum ab deleniti sunt voluptatum sit necessitatibus aliquam cumque sint totam itaque ad harum obcaecati officiis corrupti distinctio. Deserunt veniam cumque pariatur exercitationem aut ipsam obcaecati consequatur a.</p>
+                <p className={equipmentsDisplayed ? 'equipements__content' : 'hidden'}>
+                        {appartment.equipments.map((equipment) => (
+                        <li key={equipment}>{equipment}</li>
+                        ))}
+                </p>
             </div>
         </div>
     </div>
